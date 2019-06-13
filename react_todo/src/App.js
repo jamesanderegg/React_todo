2ac02cd6de4fe50985acd74885c2f052658eea32
Juicyjames <iceandjames@gmail.com>
2019-06-12 22:29:53 -0600

    Add Todo works and generates random id with uuid

--- react_todo/src/App.js
+++ react_todo/src/App.js
@@ -3,6 +3,7 @@ import Header from './components/layout/Header';
 import Todos from './components/Todos';
 import AddTodo from './components/AddTodo';
 
+import uuid from 'uuid';
 import './App.css';
 
 
@@ -11,17 +12,17 @@ class App extends Component{
   state= {
     todos: [
       {
-        id: 1,
+        id: uuid.v4(),
         title: 'Take out the Trash',
         completed: false
       },
       {
-        id: 2,
+        id: uuid.v4(),
         title: 'Dinner with wife',
         completed: false
       },
       {
-        id: 3,
+        id: uuid.v4(),
         title: 'Meeting with boss',
         completed: false
       }
@@ -46,13 +47,25 @@ class App extends Component{
       todo => todo.id !== id
     )]})
   }
+  //Add Todo Submit Button
+  addTodo = (title) => {
+    const newTodo = {
+      id: uuid.v4(),
+      title,
+      completed: false
+    }
+    this.setState({
+      todos: [...this.state.todos, newTodo]
+    });
+  }
+
   render(){
     return(
       <div className="App">
         <div className="container">
           <Header />
           <h1>App</h1>
-          <AddTodo />
+          <AddTodo addTodo={this.addTodo}/>
           <Todos todos={this.state.todos} 
           markComplete={this.markComplete}
           delTodo={this.delTodo}/>
