e2cfb11244008b656e5df31c3907fc493bcd5eba
Juicyjames <iceandjames@gmail.com>
2019-06-12 21:15:51 -0600

    Added the first components and a test array

--- react_todo/src/App.js
+++ react_todo/src/App.js
@@ -1,13 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Todos from './components/Todos'
 
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <hi>APP</hi>
-    </div>
-  );
-}
 
+
+class App extends Component{
+  state= {
+    todos: [
+      {
+        id: 1,
+        title: 'Take out the Trash',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Dinner with wife',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'Meeting with boss',
+        completed: false
+      }
+    ]
+  }
+  render(){
+    return(
+      <div className="App">
+        <h1>App</h1>
+        <Todos todos={this.state.todos}/>
+      </div>
+    )
+  }
+}
 export default App;
